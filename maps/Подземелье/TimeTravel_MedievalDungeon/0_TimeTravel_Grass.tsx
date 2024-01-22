de3b32c00f2e2fce828ec761db60e4be74ce7786
ваня помидоры <iskanyeban@gmail.com>
2024-01-22 21:18:22 +0500

    second level

---
+++ maps/Подземелье/TimeTravel_MedievalDungeon/0_TimeTravel_Grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="0_TimeTravel_Grass" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="../0_TimeTravel_Grass.png" width="64" height="32"/>
+</tileset>
---
+++ maps/Подземелье/TimeTravel_MedievalDungeon/zombieRat6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="zombieRat6" tilewidth="96" tileheight="96" tilecount="1" columns="1">
+ <image source="../zombieRat6.png" width="96" height="96"/>
+</tileset>
